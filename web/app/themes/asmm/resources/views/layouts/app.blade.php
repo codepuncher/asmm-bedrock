114f616edd1e939766aee956fcc339809b2fa08d
codepuncher <lee.hanbury@gmail.com>
2021-10-20 22:52:56 +0100

    feat: update containers

--- web/app/themes/asmm/resources/views/layouts/app.blade.php
+++ web/app/themes/asmm/resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 @include('partials.header')
 
-<div class="max-w-7xl mx-auto">
+<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
 
   <a class="sr-only focus:not-sr-only" href="#main">
     {{ __('Skip to content') }}
--- web/app/themes/asmm/resources/views/partials/footer.blade.php
+++ web/app/themes/asmm/resources/views/partials/footer.blade.php
@@ -1,3 +1,5 @@
 <footer class="bg-gray-300">
-  @php(dynamic_sidebar('sidebar-footer'))
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    @php(dynamic_sidebar('sidebar-footer'))
+  </div>
 </footer>
