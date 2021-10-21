6ea0d2f84bafdb5fd211da7d7fa93b0e30ee66fb
codepuncher <lee.hanbury@gmail.com>
2021-10-21 21:45:27 +0100

    feat: mobile menu toggle

--- web/app/themes/asmm/resources/scripts/app.js
+++ web/app/themes/asmm/resources/scripts/app.js
@@ -1,8 +1,9 @@
-/**
- * External Dependencies
- */
-import 'jquery';
-
-$(() => {
-  // console.log('Hello world');
+const mobileMenuElement = document.getElementById('menu-mobile');
+const mobileMenuToggleButton = document.getElementById('menu-mobile-toggle');
+mobileMenuToggleButton.addEventListener('click', e => {
+  if (mobileMenuElement.classList.contains('hidden')) {
+    mobileMenuElement.classList.remove('hidden');
+  } else {
+    mobileMenuElement.classList.add('hidden');
+  }
 });
--- web/app/themes/asmm/resources/views/partials/menu-mobile.blade.php
+++ web/app/themes/asmm/resources/views/partials/menu-mobile.blade.php
@@ -1,5 +1,5 @@
 <!-- Mobile menu, show/hide based on menu state. -->
-<div class="sm:hidden" id="mobile-menu">
+<div class="hidden" id="menu-mobile">
   <div class="px-2 pt-2 pb-3 space-y-1">
     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
     <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
