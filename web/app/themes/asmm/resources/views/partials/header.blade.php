05c5bfb22440a59575f9694d1da736f3415244c3
codepuncher <lee.hanbury@gmail.com>
2021-10-23 03:46:00 +0100

    cleanup header

--- web/app/themes/asmm/resources/views/partials/header.blade.php
+++ web/app/themes/asmm/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header id="header">
+<header id="header" class="bg-blue-dark relative">
   <nav class="nav-primary py-5">
     <div class="max-w-5xl mx-auto p-2 sm:px-6 lg:px-8">
       <div class="relative flex items-start justify-between">
@@ -6,8 +6,8 @@
 
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
           <div class="flex-shrink-0 flex items-center">
-            <a class="brand sm:hidden" href="{{ home_url('/') }}">ASMM</a>
-            <a class="brand hidden sm:block" href="{{ home_url('/') }}">{{ $siteName }}</a>
+            <a class="brand text-3xl font-medium text-white sm:hidden" href="{{ home_url('/') }}">ASMM</a>
+            <a class="brand text-3xl font-medium text-white hidden sm:block" href="{{ home_url('/') }}">{{ $siteName }}</a>
           </div>
           <div class="hidden sm:block sm:ml-6">
             @include('partials.nav.desktop')
