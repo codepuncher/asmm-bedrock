98d148c458ad4be584444623450ec3c2dca81cc8
codepuncher <lee.hanbury@gmail.com>
2021-11-02 21:48:11 +0000

    feat: contain page

--- web/app/themes/asmm/resources/views/layouts/app.blade.php
+++ web/app/themes/asmm/resources/views/layouts/app.blade.php
@@ -1,21 +1,17 @@
 @include('partials.header')
 
-<div class="w-full max-w-5xl mx-auto px-2 sm:px-6 lg:px-8">
+<a class="sr-only focus:not-sr-only" href="#main">
+  {{ __('Skip to content') }}
+</a>
 
-  <a class="sr-only focus:not-sr-only" href="#main">
-    {{ __('Skip to content') }}
-  </a>
+<main id="main" class="py-8 max-w-none main">
+  @yield('content')
+</main>
 
-  <main id="main" class="py-8 max-w-none main">
-    @yield('content')
-  </main>
-
-  @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-  @endif
-
-</div>
+@hasSection('sidebar')
+  <aside class="sidebar">
+    @yield('sidebar')
+  </aside>
+@endif
 
 @include('partials.footer')
