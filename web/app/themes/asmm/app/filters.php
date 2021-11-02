7bc840bb57c8c9590cb7cbef689afb1cdbb3fffd
codepuncher <lee.hanbury@gmail.com>
2021-11-02 22:23:38 +0000

    feat: add body bg and text colours

--- web/app/themes/asmm/app/filters.php
+++ web/app/themes/asmm/app/filters.php
@@ -16,3 +16,10 @@ namespace App;
 add_filter('excerpt_more', static function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'asmm'));
 });
+
+add_filter('body_class', function (array $classes): array {
+    $classes[] = 'bg-blue-dark';
+    $classes[] = 'text-white';
+
+    return $classes;
+});
