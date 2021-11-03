7eaad2e8f2a3666cb30de3513352d3e11dc45c64
codepuncher <lee.hanbury@gmail.com>
2021-11-03 19:25:30 +0000

    feat: add pull live db script

---
+++ pull-live-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REMOTE_DOMAIN='secularmeditation.me'
+LOCAL_DOMAIN='asmm.test'
+LOCAL_PATH="$(pwd)/bedrock/web/wp"
+
+wp db reset --yes && \
+wp @production db export - | \
+wp db import - && \
+wp search-replace "$REMOTE_DOMAIN" "$LOCAL_DOMAIN"
