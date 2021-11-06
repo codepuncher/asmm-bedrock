f11a47c0430a871c5795e349847127a97da93be6
codepuncher <lee.hanbury@gmail.com>
2021-11-06 02:52:39 +0000

    feat: add breadcrumbs

--- web/app/themes/asmm/resources/views/layouts/app.blade.php
+++ web/app/themes/asmm/resources/views/layouts/app.blade.php
@@ -1,5 +1,13 @@
 @include('partials.header')
 
+@if(function_exists('yoast_breadcrumb') && ! is_front_page())
+  <div class="breadcrumbs w-full flex justify-center border-b border-yellow">
+    <div class="w-full max-w-screen-xl py-8 px-2 sm:px-6 lg:px-8">
+      {!! yoast_breadcrumb() !!}
+    </div>
+  </div>
+@endif
+
 <a class="sr-only focus:not-sr-only" href="#main">
   {{ __('Skip to content') }}
 </a>
