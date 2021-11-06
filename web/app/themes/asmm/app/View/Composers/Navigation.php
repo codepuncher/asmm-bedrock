98c2e0131dccf4d3927d8034e038c4de68f1ce35
codepuncher <lee.hanbury@gmail.com>
2021-11-06 01:16:11 +0000

    style: nav changes

--- web/app/themes/asmm/app/View/Composers/Navigation.php
+++ web/app/themes/asmm/app/View/Composers/Navigation.php
@@ -42,6 +42,35 @@ class Navigation extends Composer
             return [];
         }
 
-        return Navi::build()->toArray();
+        return array_map([$this, 'setItemClasses'], Navi::build()->toArray());
+    }
+
+    protected function setChildItemClasses(object $item, int $index, object $parent): object
+    {
+
+        return $item;
+    }
+
+    protected function setItemClasses(object $parent): object
+    {
+        $baseClasses = 'text-white px-3 py-2 text-sm font-medium transition-colors border-b-2 border-transparent hover:text-green hover:border-green';
+        $activeClasses = "{$baseClasses} border-green";
+        $inactiveClasses = "{$baseClasses} hover:border-b hover:border-green";
+        $additionalParentclasses = $parent->active ? $activeClasses : $inactiveClasses;
+        $parent->classes .= " {$additionalParentclasses}";
+
+        if (empty($parent->children)) {
+            return $parent;
+        }
+
+        $parentChildrenCount = count($parent->children);
+        $parent->children = array_map(function (object $item, int $index) use ($parentChildrenCount): object {
+            $baseClasses = 'bg-white text-green text-sm block p-3 hover:bg-green hover:text-white';
+            $item->classes .= " {$baseClasses}";
+
+            return $item;
+        }, array_values($parent->children), array_keys($parent->children));
+
+        return $parent;
     }
 }
--- web/app/themes/asmm/resources/views/partials/nav/desktop.blade.php
+++ web/app/themes/asmm/resources/views/partials/nav/desktop.blade.php
@@ -4,11 +4,7 @@
       <li class="{{ $loop->first ? '' : 'ml-3' }}">
         <a
           href="{{ $item->url }}"
-          @if($item->active)
-          class="bg-green text-white px-3 py-2 rounded-md text-sm font-medium"
-          @else
-          class="text-white hover:bg-green hover:text-white px-3 py-2 rounded-md text-sm font-medium transition-colors"
-          @endif
+          class="{{ $item->classes }}"
           aria-current="page"
         >{!! esc_html($item->label) !!}</a>
       </li>
@@ -16,11 +12,7 @@
       <li class="group relative dropdown ml-3">
         <a
           href="{{ $item->url }}"
-          @if($item->active)
-          class="bg-green text-white px-3 py-2 rounded-md text-sm font-medium"
-          @else
-          class="text-white group-hover:bg-green px-3 py-2 rounded-md text-sm font-medium transition-colors"
-          @endif
+          class="{{ $item->classes }}"
           aria-current="page"
         >{!! esc_html($item->label) !!}</a>
 
@@ -31,13 +23,13 @@
                 <a
                   href="{{ $child->url }}"
                   @if($loop->first && $loop->last)
-                    class="bg-white text-green text-sm block p-3 hover:bg-green hover:text-white transition-colors rounded"
+                    class="{{ $child->classes }} rounded"
                   @elseif($loop->first)
-                    class="bg-white text-green text-sm block p-3 hover:bg-green hover:text-white transition-colors rounded-t"
+                    class="{{ $child->classes }} rounded-t"
                   @elseif($loop->last)
-                    class="bg-white text-green text-sm block p-3 hover:bg-green hover:text-white transition-colors rounded-b"
+                    class="{{ $child->classes }} rounded-b"
                   @else
-                    class="bg-white text-green text-sm block p-3 hover:bg-green hover:text-white"
+                    class="{{ $child->classes }}"
                   @endif
                 >{!! esc_html($child->label) !!}</a>
               </li>
