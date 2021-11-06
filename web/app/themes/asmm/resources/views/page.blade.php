860aa365b7c7efdd96ec149b1bd576a4fa5d30e1
codepuncher <lee.hanbury@gmail.com>
2021-11-06 02:52:07 +0000

    feat: hide header on front page

--- web/app/themes/asmm/resources/views/page.blade.php
+++ web/app/themes/asmm/resources/views/page.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
+    @includeUnless(is_front_page(), 'partials.page-header')
     @includeFirst(['partials.content-page', 'partials.content'])
   @endwhile
 @endsection
