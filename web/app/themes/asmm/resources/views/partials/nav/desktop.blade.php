4e9b1e272f428fae8aaf374f0aacbe3e9d540b5e
codepuncher <lee.hanbury@gmail.com>
2023-11-11 22:19:12 +0000

    header style changes

--- web/app/themes/asmm/resources/views/partials/nav/desktop.blade.php
+++ web/app/themes/asmm/resources/views/partials/nav/desktop.blade.php
@@ -1,36 +1,34 @@
-<ul class="flex justify-between">
-  @foreach($items as $item)
+<ul class="mb-0 flex list-none justify-between">
+  @foreach ($items as $item)
     @empty($item->children)
-      <li class="{{ $loop->first ? '' : 'ml-3' }}">
+      <li class="mb-0 {{ $loop->first ? '' : 'ml-3' }}">
         <a
-          href="{{ $item->url }}"
           class="{{ $item->classes }}"
+          href="{{ $item->url }}"
           aria-current="page"
         >{!! esc_html($item->label) !!}</a>
       </li>
     @else
-      <li class="group relative dropdown ml-3">
+      <li class="dropdown group relative mb-0 ml-3">
         <a
+          class="{{ $item->classes }} hover:before:content-none"
           href="{{ $item->url }}"
-          class="hover:before:content-none {{ $item->classes }}"
           aria-current="page"
         >{!! esc_html($item->label) !!}</a>
 
-        <div class="hidden absolute group-hover:block dropdown-menu h-auto shadow w-48 pt-3">
-          <ul>
-            @foreach($item->children as $child)
-              <li>
+        <div class="dropdown-menu absolute hidden h-auto w-48 pt-3 shadow group-hover:block">
+          <ul class="list-none">
+            @foreach ($item->children as $child)
+              <li class="mb-0">
                 <a
                   href="{{ $child->url }}"
-                  @if($loop->first && $loop->last)
-                    class="{{ $child->classes }} rounded"
+                  @if ($loop->first && $loop->last) class="{{ $child->classes }} rounded"
                   @elseif($loop->first)
                     class="{{ $child->classes }} rounded-t"
                   @elseif($loop->last)
                     class="{{ $child->classes }} rounded-b"
                   @else
-                    class="{{ $child->classes }}"
-                  @endif
+                    class="{{ $child->classes }}" @endif
                 >{!! esc_html($child->label) !!}</a>
               </li>
             @endforeach
